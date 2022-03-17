429c4d9c1fc4eedacec48fcce383049feffbf863
thomas-ambrogini <thomas.ambrogini@studio.unibo.it>
2022-03-17 17:07:54 +0100

    style: change spaces into tabs

--- it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/DeviceFactory.java
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/DeviceFactory.java
@@ -11,41 +11,32 @@ import it.unibo.radarSystem22.domain.utils.DomainSystemConfig;
 
 public class DeviceFactory {
 
-    public static ILed createLed() {
-            ColorsOut.out("DeviceFactory | createLed simulated="+DomainSystemConfig.simulation, ColorsOut.GREEN);
-            if( DomainSystemConfig.simulation)  {
-                    return LedModel.createLedMock();
-            }else {
-                    return LedModel.createLedConcrete();
-            }
+	public static ILed createLed() {
+		ColorsOut.out("DeviceFactory | createLed simulated="+DomainSystemConfig.simulation, ColorsOut.GREEN);
+		if( DomainSystemConfig.simulation)  {
+			return LedModel.createLedMock();
+		}else {
+			return LedModel.createLedConcrete();
+		}
     }
-    public static ISonar createSonar(boolean observable) {
-            ColorsOut.out("DeviceFactory | createSonar simulated="+DomainSystemConfig.simulation, ColorsOut.GREEN);
-//          if( observable ) return createSonarObservable();
-//          else 
-                    return createSonar();
-    }
+	
+	public static ISonar createSonar(boolean observable) {
+		ColorsOut.out("DeviceFactory | createSonar simulated="+DomainSystemConfig.simulation, ColorsOut.GREEN);
+		return createSonar();
+	}
 
-    public static ISonar createSonar() {
-            ColorsOut.out("DeviceFactory | createSonar simulated="+DomainSystemConfig.simulation);
-            if( DomainSystemConfig.simulation)  {
-                    return SonarModel.createSonarMock();
-            }else {
-                    return SonarModel.createSonarConcrete();
-            }
-    }
-//  public static ISonarObservable createSonarObservable() {
-//          ColorsOut.out("DeviceFactory | createSonarObservable simulated="+DomainSystemConfig.simulation);
-//          if( DomainSystemConfig.simulation)  {
-//                  return new SonarMockObservable();
-//          }else { 
-//                  return new SonarConcreteObservable();
-//          }       
-//  }
+	public static ISonar createSonar() {
+	    ColorsOut.out("DeviceFactory | createSonar simulated="+DomainSystemConfig.simulation);
+	    if( DomainSystemConfig.simulation)  {
+	            return SonarModel.createSonarMock();
+	    }else {
+	            return SonarModel.createSonarConcrete();
+	    }
+	}
 
-    //We do not have mock for RadarGui
-    public static IRadarDisplay createRadarGui() {
-            return RadarDisplay.getRadarDisplay();
-    }
+	//We do not have mock for RadarGui
+	public static IRadarDisplay createRadarGui() {
+	    return RadarDisplay.getRadarDisplay();
+	}
 
 }
